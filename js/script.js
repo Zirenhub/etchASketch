be1f6b4403aa4ebd0ca16e2c466910b16f911c17
Erdinch <erdinchorhan02@gmail.com>
2022-07-20 16:53:21 +0300

    rainbow mode

--- js/script.js
+++ js/script.js
@@ -1,40 +1,3 @@
-// function draw() {
-//   gridContainer.replaceChildren();
-//   for (let i = 0; i < Number(gridValue) * Number(gridValue); i++) {
-//     let grid = document.createElement('div');
-//     grid.classList.add('cell');
-//     grid.addEventListener('mouseover', (e) => {
-//       e.target.setAttribute('style', 'background-color:black;');
-//     });
-//     gridContainer.appendChild(grid);
-//   }
-//   gridContainer.style.gridTemplateColumns = `repeat(${gridValue}, auto)`;
-//   gridContainer.style.gridTemplateRows = `repeat(${gridValue}), auto)`;
-// }
-
-// function buttonToggle() {
-//   if (rainbowButton.value == 'Rainbow Mode : OFF') {
-//     rainbowButton.value = 'Rainbow Mode : ON';
-//     const getRandomNumber = (maxNum) => {
-//       return Math.floor(Math.random() * maxNum);
-//     };
-//     const getRandomColor = () => {
-//       const h = getRandomNumber(360);
-//       const s = getRandomNumber(100);
-//       const l = getRandomNumber(100);
-
-//       return `hsl(${h}deg, ${s}%, ${l}%)`;
-//     };
-
-//     const randomColorPick = getRandomColor();
-//     return randomColorPick;
-//   } else if (rainbowButton.value == 'Rainbow Mode : ON') {
-//     rainbowButton.value = 'Rainbow Mode : OFF';
-//   }
-// }
-
-// let randomColor = buttonToggle();
-
 const gridContainer = document.querySelector('.grid-container');
 const rowToot = document.getElementById('sizeSlider');
 const colorPicker = document.getElementById('colorPicker');
@@ -57,14 +20,16 @@ function watchColorPicker(e) {
   console.log(colorPicked);
 }
 
-colorPicker.addEventListener('change', watchColorPicker, false);
-
-clearButton.addEventListener('click', () => {
+function clearGrid() {
   const removeCells = document.querySelectorAll('.cell');
   for (let i = 0; i < removeCells.length; i++) {
     removeCells[i].style.backgroundColor = '';
   }
-});
+}
+
+colorPicker.addEventListener('change', watchColorPicker, false);
+
+clearButton.addEventListener('click', clearGrid);
 
 function createGrid() {
   gridContainer.replaceChildren();
@@ -77,13 +42,61 @@ function createGrid() {
   gridContainer.style.gridTemplateRows = `repeat(${gridValue}), auto)`;
 }
 
-rowToot.onchange = () => {
-  createGrid();
-  const getCells = document.querySelectorAll('.cell');
+function buttonToggle() {
+  if (rainbowButton.value == 'Rainbow Mode : OFF') {
+    rainbowButton.value = 'Rainbow Mode : ON';
+    rainbowMode();
+  } else if (rainbowButton.value == 'Rainbow Mode : ON') {
+    rainbowButton.value = 'Rainbow Mode : OFF';
+    normalMode();
+  }
+}
 
+function normalMode() {
+  clearGrid();
+  const getCells = document.querySelectorAll('.cell');
   for (let i = 0; i < getCells.length; i++) {
     getCells[i].addEventListener('mouseover', () => {
       getCells[i].style.backgroundColor = colorPicked;
     });
   }
+}
+
+function rainbowMode() {
+  clearGrid();
+  const getCells = document.querySelectorAll('.cell');
+  if (rainbowButton.value == 'Rainbow Mode : ON') {
+    for (let i = 0; i < getCells.length; i++) {
+      getCells[i].addEventListener('mouseover', () => {
+        getRandomRainbowColor();
+        let randomColor = getRandomRainbowColor();
+        getCells[i].style.backgroundColor = randomColor;
+      });
+    }
+  }
+}
+
+function getRandomRainbowColor() {
+  const getRandomNumber = (maxNum) => {
+    return Math.floor(Math.random() * maxNum);
+  };
+  const getRandomColor = () => {
+    const h = getRandomNumber(360);
+    const s = getRandomNumber(100);
+    const l = getRandomNumber(100);
+
+    return `hsl(${h}deg, ${s}%, ${l}%)`;
+  };
+
+  const randomColorPick = getRandomColor();
+  return randomColorPick;
+}
+
+rowToot.onchange = () => {
+  createGrid();
+  if (rainbowButton.value == 'Rainbow Mode : ON') {
+    rainbowMode();
+    return;
+  }
+  normalMode();
 };
